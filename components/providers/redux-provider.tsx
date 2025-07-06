3b200c623afbfddf0f1b46a3d587432d36b401c2
wptasmina <wptasmina@gmail.com>
2025-07-06 16:59:52 +0600

    store add

--- components/providers/redux-provider.tsx
+++ components/providers/redux-provider.tsx
@@ -1,14 +1,16 @@
+// File: components/providers/redux-provider.tsx
 "use client"
 
-import type React from "react"
-
+import React from "react"
 import { Provider } from "react-redux"
 import { store } from "@/lib/store/store"
 
-export function ReduxProvider({ children }: { children: React.ReactNode }) {
-  return 
-  <Provider store={store}>
-    
-    {children}
-    </Provider>
+
+type Props = {
+  children: React.ReactNode
 }
+
+export const ReduxProvider = ({ children }: Props) => {
+  return <Provider store={store}>{children}</Provider>
+}
+
